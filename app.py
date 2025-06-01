7c9bbb22f1a10e6888bd39619af712bcc3b6b0e1
OzvalDoRJ4552 <167720238+OzvalDoRJ4552@users.noreply.github.com>
2025-06-01 09:46:23 -0500

    initial commit

--- app.py
+++ app.py
@@ -3,4 +3,10 @@ import streamlit as st
 
 st.header('Tossing a Coin ')
 
+number_of_trails = st.slider('Number of trials?', 1, 1000, 10)
+start_button = st.button('Run')
+
+if start_button:
+    st.write(f'Running the expiriment of {number_of_trails} trials')
+
 st.write('It is not a functional application yet. Under construction.:')
